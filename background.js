d6a201a09c78caa9e020cf73a6c506a4b1c379ea
Emanuel Zhupa <ezhupa@helius-software.com>
2021-04-07 03:45:33 +0200

    change logic on what to listen

--- background.js
+++ background.js
@@ -1,16 +1,39 @@
-chrome.tabs.onActivated.addListener(tab => {
-    chrome.tabs.get(tab.tabId, currentTab => {
-        if (/^https:\/\/www\.filma24/.test(currentTab.url)) {
-            chrome.tabs.executeScript(null, {file: './foreground.js'}, () => {
-            })
-        }
+function checkIfGoodLinkAndWork(result, details) {
+    const isGoodLink = result.goodLinks.some(link => {
+        return link.includes(details.url);
     })
-})
 
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-        if (/^https:\/\/www\.filma24/.test(tab.url)) {
-            chrome.tabs.executeScript(null, {file: './foreground.js'}, () => {
-            })
-        }
+    if (!isGoodLink) {
+        chrome.tabs.executeScript(details.tabId, {file: './removeAddsAndShowFromSource.js'}, () => {
+        })
     }
-)
+}
+
+chrome.webRequest.onCompleted.addListener(
+    function (details) {
+        console.log(details);
+
+        if (details.url === "https://www.filma24.ai/") {
+            chrome.tabs.executeScript(details.tabId, {file: './removeAddsFromHome.js'}, () => {
+            })
+            return {cancel: false};
+        }
+
+        chrome.storage.sync.get(['goodLinks'], function (result) {
+
+            if (!result?.goodLinks?.length) {
+                chrome.tabs.executeScript(details.tabId, {file: './fillGoodLinks.js'}, () => {
+                })
+
+                chrome.storage.sync.get('goodLinks', function (res){
+                    checkIfGoodLinkAndWork(res, details);
+                })
+            }
+
+            checkIfGoodLinkAndWork(result, details);
+        })
+
+        return {cancel: false};
+    },
+    {urls: ["*://www.filma24.ai/*"], types: ["main_frame"]},
+);
