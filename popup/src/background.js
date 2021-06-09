a9847b13f4ee4a519344b4816275b2b70fa92181
Emanuel Zhupa <ezhupa@helius-software.com>
2021-06-09 02:38:18 +0200

    add logic to sync values with Chrome Storage API

--- popup/src/background.js
+++ popup/src/background.js
@@ -1,6 +1,30 @@
-let color = '#3aa757';
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.storage.sync.set({
+        ads: true,
+        redirects: true
+    });
+});
 
-// chrome.runtime.onInstalled.addListener(() => {
-//     chrome.storage.sync.set({color});
-//     console.log('Default background color set to %cgreen', `color: ${color}`);
-// });
+let ads = true,
+    redirects = true;
+
+chrome.storage.sync.get(['ads', 'redirects'], (result) => {
+    ads = result.ads;
+    redirects = result.redirects;
+});
+
+chrome.storage.onChanged.addListener((changes, namespace) => {
+    for (let [key, {oldValue, newValue}] of Object.entries(changes)) {
+
+        if (namespace === "sync") {
+            // * Set the local variables the new value
+            this[key] = changes[key].newValue;
+
+            console.log(
+                `Storage key "${key}" in namespace "${namespace}" changed.`,
+                `Old value was "${oldValue}", new value is "${newValue}".`
+            );
+        }
+    }
+});
+
