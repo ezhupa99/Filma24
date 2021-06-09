19c9ecc439dabba68e9fb6e72efa8772c42b7453
Emanuel Zhupa <ezhupa@helius-software.com>
2021-06-09 02:37:46 +0200

    add logic on how to get and set values from Chrome API inside Angular Component

--- popup/src/app/app.component.ts
+++ popup/src/app/app.component.ts
@@ -1,9 +1,28 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
     selector: 'app-root',
     templateUrl: "app.component.html",
     styleUrls: ["app.component.scss"]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+    blockAds = true;
+    blockRedirects = true;
+
+    ngOnInit() {
+        // @ts-ignore
+        chrome.storage.sync.get(['ads', 'redirects'], (result) => {
+            this.blockAds = result.ads;
+            this.blockRedirects = result.redirects;
+        });
+    }
+
+    onChromeUpdateSync(state: boolean, field: string) {
+        // @ts-ignore
+        chrome.storage.sync.set({
+            [field]: state
+        }, () => {
+            console.log("Updated " + field + " with state: " + state)
+        });
+    }
 }
