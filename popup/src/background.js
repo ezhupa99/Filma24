d2611eb447cdd637ecc8ebb62b8e1ac413cfc4e8
Emanuel Zhupa <ezhupa@helius-software.com>
2021-06-14 05:02:40 +0200

    change logic of background because some events are sent thru popup directly to contentScript

--- popup/src/background.js
+++ popup/src/background.js
@@ -1,48 +1,58 @@
+"use strict";
+
 chrome.runtime.onInstalled.addListener(() => {
+
+    // * Set default values of the extension
     chrome.storage.sync.set({
         ads: true,
         redirects: true
     });
-});
 
-let ads = true,
-    redirects = true;
 
-chrome.storage.sync.get(['ads', 'redirects'], (result) => {
-    ads = result.ads;
-    redirects = result.redirects;
-});
-
-chrome.storage.onChanged.addListener(async (changes, namespace) => {
-    for (let [key, {oldValue, newValue}] of Object.entries(changes)) {
-
-        if (namespace === "sync") {
-            // * Set the local variables the new value
-            this[key] = changes[key].newValue;
-            const tab = await getTab();
-
-            switch (key) {
-                case "ads":
-                    chrome.scripting.executeScript({
-                        target: {tabId: tab.id},
-                        function: removeRealAds,
-                    });
-                    break;
-
+    // * Reload all tabs to make sure popup and content script message API works
+    chrome.tabs.query({}, function (tabs) {
+        tabs.forEach(
+            (tab) => {
+                if (tab.url.includes("filma24")) {
+                    chrome.tabs.reload(tab.id);
+                }
             }
-        }
-    }
+        )
+    })
 });
 
-getTab =
-    async () => {
-        const [tab] =
-            await chrome.tabs.query(
-                {
-                    active: true,
-                    currentWindow: true
-                });
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo.status === "complete") {
 
-        console.log("Inside GetTab: ", tab)
-        return tab;
+        chrome.storage.sync.get(
+            ["ads", "redirects"],
+            function (res) {
+                chrome.tabs.sendMessage(tabId, {
+                    state: res.ads,
+                    field: "Ads"
+                })
+
+                chrome.tabs.sendMessage(tabId, {
+                    state: res.redirects,
+                    field: "Redirects"
+                })
+            })
+
+
     }
+})
+
+
+// chrome.storage.onChanged.addListener(async (changes, namespace) => {
+//     for (let [key, {oldValue, newValue}] of Object.entries(changes)) {
+//
+//         if (namespace === "sync") {
+//             // * Set the local variables the new value
+//             if (key === "ads") {
+//                 ads = newValue;
+//             } else if (key === "redirects") {
+//                 redirects = newValue;
+//             }
+//         }
+//     }
+// });
