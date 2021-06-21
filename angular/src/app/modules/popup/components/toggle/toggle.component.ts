92bdbbe776a0777d814d6a35909d5d3d6f3b71e3
Emanuel Zhupa <ezhupa@helius-software.com>
2021-06-22 00:19:29 +0200

    add toggle component

---
+++ angular/src/app/modules/popup/components/toggle/toggle.component.ts
@@ -0,0 +1,18 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+
+@Component({
+    selector: 'app-toggle',
+    templateUrl: './toggle.component.html',
+    styleUrls: ['./toggle.component.sass']
+})
+export class ToggleComponent {
+
+    @Input() active = null;
+    @Output() state = new EventEmitter<boolean>(false);
+
+    onStateChange(e: Event): void {
+        const el = e.target as HTMLInputElement;
+        // * Emit new state
+        this.state.emit(el.checked)
+    }
+}
