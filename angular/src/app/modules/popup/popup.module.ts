c5e0d03fa1f065627fb14d3e0848d96396f203c6
Emanuel Zhupa <ezhupa@helius-software.com>
2021-06-22 00:18:46 +0200

    add footer and toggle component on popup module

--- angular/src/app/modules/popup/popup.module.ts
+++ angular/src/app/modules/popup/popup.module.ts
@@ -2,9 +2,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { PopupComponent } from './pages/popup/popup.component';
 import { PopupRoutingModule } from './popup-routing.module';
+import { FooterComponent } from './components/footer/footer.component';
+import { ToggleComponent } from './components/toggle/toggle.component';
 
 @NgModule({
-  declarations: [PopupComponent],
+  declarations: [PopupComponent, FooterComponent, ToggleComponent],
   imports: [CommonModule, PopupRoutingModule]
 })
 export class PopupModule {}
