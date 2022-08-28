7332c8d9d85a006f3674a4cbc64a157296042b46
Emanuel Zhupa <ezhupa99@gmail.com>
2022-08-28 05:45:10 +0200

    update webpack configs

--- chrome/webpack.config.js
+++ chrome/webpack.config.js
@@ -1,28 +1,29 @@
-const { CheckerPlugin } = require('awesome-typescript-loader');
 const { join } = require('path');
 
 module.exports = {
-  mode: 'development',
-  devtool: 'inline-source-map',
-  entry: {
-    contentPage: join(__dirname, 'src/contentPage.ts'),
-    serviceWorker: join(__dirname, 'src/serviceWorker.ts')
-  },
-  output: {
-    path: join(__dirname, '../angular/dist'),
-    filename: '[name].js'
-  },
-  module: {
-    rules: [
-      {
-        exclude: /node_modules/,
-        test: /\.ts?$/,
-        use: 'awesome-typescript-loader?{configFileName: "chrome/tsconfig.json"}'
-      }
-    ]
-  },
-  plugins: [new CheckerPlugin()],
-  resolve: {
-    extensions: ['.ts', '.js']
-  }
+    mode: 'development',
+    devtool: 'inline-source-map',
+    entry: {
+        contentPage: join(__dirname, 'src/contentPage.ts'),
+        serviceWorker: join(__dirname, 'src/serviceWorker.ts')
+    },
+    output: {
+        path: join(__dirname, '../angular/dist'),
+        filename: '[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts?$/,
+                exclude: /node_modules/,
+                loader: "ts-loader",
+                options: {
+                    configFile: "chrome/tsconfig.json",
+                }
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    }
 };
--- chrome/webpack.config.prod.js
+++ chrome/webpack.config.prod.js
@@ -1,32 +1,29 @@
-const { CheckerPlugin } = require('awesome-typescript-loader');
 const { join } = require('path');
-const { optimize } = require('webpack');
+const { webpack } = require('webpack');
 
 module.exports = {
-  mode: 'production',
-  entry: {
-    contentPage: join(__dirname, 'src/contentPage.ts'),
-    serviceWorker: join(__dirname, 'src/serviceWorker.ts')
-  },
-  output: {
-    path: join(__dirname, '../angular/dist'),
-    filename: '[name].js'
-  },
-  module: {
-    rules: [
-      {
-        exclude: /node_modules/,
-        test: /\.ts?$/,
-        use: 'awesome-typescript-loader?{configFileName: "chrome/tsconfig.json"}'
-      }
-    ]
-  },
-  plugins: [
-    new CheckerPlugin(),
-    new optimize.AggressiveMergingPlugin(),
-    new optimize.OccurrenceOrderPlugin()
-  ],
-  resolve: {
-    extensions: ['.ts', '.js']
-  }
+    mode: 'production',
+    entry: {
+        contentPage: join(__dirname, 'src/contentPage.ts'),
+        serviceWorker: join(__dirname, 'src/serviceWorker.ts')
+    },
+    output: {
+        path: join(__dirname, '../angular/dist'),
+        filename: '[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts?$/,
+                exclude: /node_modules/,
+                loader: "ts-loader",
+                options: {
+                    configFile: "chrome/tsconfig.json",
+                }
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    }
 };
